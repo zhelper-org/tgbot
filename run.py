3cf474f8e968a01ac069c9ac15df01a93806bde0
z4g5yR <z4g5yR@proton.me>
2022-12-15 15:52:57 +0000

    init

---
+++ run.py
@@ -0,0 +1,21 @@
+import telebot
+import requests
+import json
+import os
+zhelper_bot_token = os.environ['zhelper_bot_token']
+bot = telebot.TeleBot(zhelper_bot_token) # You can set parse_mode by default. HTML or MARKDOWN
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Search by send '/search keywords', and get download link by send '/detail id'.")
+@bot.message_handler(commands=['search'])
+def search(message):
+    r = requests.post('https://api.v5.zhelper.net/api/search/',json={'keyword':message.text.split(' ')[-1]})
+    j = json.loads(r.text)
+    bot.reply_to(message, '\n'.join([' '.join([str(x) for x in [i['title'],i['author'],i['publisher'],i['extension'],i['filesizeString'],'/detail',i['zlibrary_id'],]]) for i in j]))
+@bot.message_handler(commands=['detail'])
+def detail(message):
+    r = requests.post('https://api.v5.zhelper.net/api/detail/',json={'id':str(message.text.split(' ')[-1])})
+    print(r.text)
+    j = json.loads(r.text)
+    bot.reply_to(message, '\n'.join(['mc_code: {}'.format(j['mc']),'ipfs_id: {}'.format(j['ipfs_cid']),'ipfs_link: https://ipfs.io/ipfs/{}'.format(j['ipfs_cid']),'ipfs_link2: https://dweb.link/ipfs/{}'.format(j['ipfs_cid']),'is_in_libgin: {}'.format(j['in_libgen'])]))
+bot.infinity_polling()
